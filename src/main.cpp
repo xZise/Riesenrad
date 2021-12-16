1af081c8303ab32bde48617f1f08dbba7dfd3148
Fabian Neundorf <CommodoreFabianus@gmx.de>
2021-12-16 16:24:18 +0100

    Debug code removed and array_size constexpr added

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+template <typename T, size_t N>
+constexpr size_t array_size(T (&)[N]) {
+    return N;
+}
+
 constexpr uint8_t NUM_LEDS = 30;
 constexpr uint8_t DATA_PIN = 8;
 
 constexpr uint32_t availableColors[] = {CRGB::Red, CRGB::Yellow, CRGB::Green, CRGB::Ivory};
-constexpr uint8_t availableColorsLength = sizeof(availableColors) / sizeof(uint32_t);
+constexpr uint8_t availableColorsLength = array_size(availableColors);
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  leds[0] = CRGB(0xf0, 0, 0);
-  leds[1] = CRGB(0, 0xf0, 0);
-  leds[2] = CRGB(0, 0, 0xf0);
-
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(30);
 }
@@ -152,7 +152,7 @@ void move4() {
 typedef void (*t_movefunc)();
 
 constexpr t_movefunc movefuncs[] = {&move1, &move2, &move3, &move4};
-constexpr uint8_t numMoveFuncs = sizeof(movefuncs) / sizeof(t_movefunc);
+constexpr uint8_t numMoveFuncs = array_size(movefuncs);
 
 void move(uint8_t mode) {
   allBlack();
