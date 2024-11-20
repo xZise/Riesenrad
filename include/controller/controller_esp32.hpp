ce34c1a6683665786ae844130e1b8fce0e74d6c8
Fabian Neundorf <CommodoreFabianus@gmx.de>
2024-11-21 00:47:24 +0100

    Add support for Home Assistant via MQTT

--- include/controller/controller_esp32.hpp
+++ include/controller/controller_esp32.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ArduinoHA.h>
+
 #include <freertos/task.h>
 #include "controller/controller.hpp"
 
@@ -9,18 +11,27 @@ namespace Ferriswheel
 template<uint8_t DATA_PIN>
 class ESP32Controller final : public Controller<DATA_PIN> {
 public:
+  void setMqtt(HAMqtt* mqtt) { _mqtt = mqtt; }
+
   virtual void setupTimer() override {
     xTaskCreatePinnedToCore(&taskLoop, "Animationloop", 2000, this, 1, nullptr, 1);
   }
 
   virtual void run() override {
-    while (true) { taskYIELD(); }
+    while (true) {
+      if (_mqtt) {
+        _mqtt->loop();
+      }
+      taskYIELD();
+    }
   }
 protected:
   virtual void delayFrame() override {
     vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 private:
+  HAMqtt* _mqtt;
+
   static void taskLoop(void* parameters) {
     static_cast<ESP32Controller<DATA_PIN>*>(parameters)->outsideLoop();
   }
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <new>
 
 #ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#include <ArduinoHA.h>
 #include <freertos/task.h>
 #endif
 
@@ -11,6 +13,7 @@
 #elif ARDUINO_AVR_UNO
 #include "controller/controller_uno.hpp"
 #elif ARDUINO_ARCH_ESP32
+#include "secrets.hpp"
 #include "controller/controller_esp32.hpp"
 #else
 #error "Board is not supported"
@@ -24,11 +27,30 @@ Ferriswheel::ArduinoMicroController<8> controller;
 Ferriswheel::ArduinoUnoController<8> controller;
 #elif ARDUINO_ARCH_ESP32
 Ferriswheel::ESP32Controller<12> controller;
+
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt(client, device);
+
+HALight animationsSwitch("animations", HALight::BrightnessFeature);
 #endif
 
 #ifdef TIMER_VEC
 ISR(TIMER_VEC) {
   controller.nextFrame();
+}
+#endif
+
+#ifdef ARDUINO_ARCH_ESP32
+void onStateCommand(bool state, HALight* sender)
+{
+  controller.setAnimationsEnabled(state);
+  sender->setState(state);
+}
+
+void onBrightnessCommand(uint8_t brightness, HALight* sender) {
+  FastLED.setBrightness(brightness);
+  sender->setBrightness(brightness); // report brightness back to the Home Assistant
 }
 #endif
 
@@ -42,6 +64,41 @@ void setup() {
 
   FastLED.addLeds<WS2812B, controller.rgbLEDpin(), GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(30);
+
+  #ifdef ARDUINO_ARCH_ESP32
+  // Unique ID must be set!
+  byte mac[6] = {0};
+  WiFi.macAddress(mac);
+  device.setUniqueId(mac, sizeof(mac));
+
+  // connect to wifi
+  WiFi.setHostname("Ferriswheel");
+  Serial.println("Start connection");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+      Serial.print('.');
+      delay(500); // waiting for the connection
+  }
+  Serial.println();
+  Serial.println("Connected to the network");
+
+  // set device's details (optional)
+  device.setName("Ferriswheel");
+  device.setSoftwareVersion("1.0.0");
+
+  // handle switch state
+  animationsSwitch.onStateCommand(onStateCommand);
+  animationsSwitch.onBrightnessCommand(onBrightnessCommand);
+  animationsSwitch.setName("Animations");
+  animationsSwitch.setState(false);
+
+  mqtt.begin(BROKER_ADDR, MQTT_USER, MQTT_PASSWORD);
+
+  controller.setMqtt(&mqtt);
+  #else
+  controller.setAnimationsEnabled(true);
+  #endif
 
   controller.setupTimer();
 
